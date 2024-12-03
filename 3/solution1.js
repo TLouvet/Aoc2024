9473612ea8b7bf36935ec0f8052b4d2abc177d5f
Louvet Thomas <thomas.louvet@laposte.net>
2024-12-03 12:12:05 +0100

    refactor: :recycle: pour le plaisir du one line

--- 3/solution1.js
+++ 3/solution1.js
@@ -1,11 +1,3 @@
 const { getInput, MUL_REGEX, getProduct } = require("./common");
 
-const lines = getInput();
-const expressions = lines.match(MUL_REGEX);
-let sum = BigInt(0); // Compte des résultats de fin, je suis pas certain que c'était nécessaire
-
-for (const expression of expressions) {
-  sum += getProduct(expression);
-}
-
-console.log("Part 1:", sum);
+console.log("Part 1:", (getInput().match(MUL_REGEX) || []).reduce((acc, curr) => acc + getProduct(curr), BigInt(0)));
